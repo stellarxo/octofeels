b1af6b0182bcfbb4ef3b2a99b31ea6c354e6bf39
Zachery Miranda <zmiranda@mit.edu>
2016-10-21 23:10:48 -0400

    bleh webcam sorta

--- client/controllers/feelCtrl.js
+++ client/controllers/feelCtrl.js
@@ -1,3 +1,14 @@
 app.controller('feelCtrl', function($scope, $http) {
-	$scope.derp = "Octofeeeeeeeels"
+	$scope.derp = "Octofeeeeeeeels";
+
+	// attachs the webcam to the camera
+	Webcam.attach('#my_camera');
+
+    $scope.take_snapshot = function() {
+        Webcam.snap( function(data_uri) {
+        	// TODO send to API
+            document.getElementById('my_result').innerHTML = '<img src="'+ data_uri+'"/>';
+        } );
+    }
+
 });
