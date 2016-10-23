9298c3af9390839d696c0b960e262b22f22770d3
Zachery Miranda <zmiranda@mit.edu>
2016-10-22 23:29:53 -0400

    add an api to get filenames

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 var express = require('express');
 var http = require('http');
+var fs = require('fs');
 
+var port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
+
+// if the port is not local, then we need https for chrome
+if(port !== 3000) {
+	var https = require('https');
+	
+	// TODO load certs
+	// var options = {
+	//   key: fs.readFileSync('cert/octofeels.tk.key'),
+	//   cert: fs.readFileSync('cert/octofeels.crt')
+	// };
+
+	// TODO actually change server to be https
+	// server = https.createServer(options, app);
+}
 
 var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({
@@ -12,6 +28,14 @@ app.use(bodyParser.urlencoded({
 // serve static files
 app.use(express.static('client'));
 
+// get all files for the current emotion
+app.get('/api/images/:emotion', function(req, res) {
+	var path = 'client/img/' + req.params.emotion
+
+	fs.readdir(path, function(err, items) {
+	    res.send(items)
+	});
+})
 
 // have all links direct to our main file
 app.all('/*', function ( req, res ) {
@@ -19,7 +43,7 @@ app.all('/*', function ( req, res ) {
     })
 
 // make the server start and listen
-server.listen(process.env.PORT || 3000, function () {
+server.listen(port, function () {
   var host = server.address().address;
   var port = server.address().port;
 
